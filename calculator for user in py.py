89da70a574592a571f6b76b523f9ada9c5b66660
Rajshirsat <156605877+Rajshirsat@users.noreply.github.com>
2024-01-16 18:17:13 +0530

    Add files via upload

---
+++ calculator for user in py.py
@@ -0,0 +1,12 @@
+#calculator with user name
+
+a = input("ENter Name For You Calculator : ")
+print(a)
+A = input("Enter First NUm :")
+B = input("Enter Second Num :")
+# A & B variables is in string type , Thats why we use typefunctions (int(A))
+
+print(int(A) + int(B))
+print(int(A) - int(B))
+print(int(A) * int(B))
+print(int(A) / int(B))
